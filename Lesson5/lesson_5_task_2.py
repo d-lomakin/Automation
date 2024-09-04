5e14802d38e40f8338ed0e2c6338c2789c656e22
d.lomakin <d.lomakin1705@gmail.com>
2024-09-04 19:29:18 +0300

    HW5

---
+++ Lesson5/lesson_5_task_2.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+
+try:
+    driver.get("http://uitestingplayground.com/dynamicid")
+
+    button = driver.find_element(
+        By.XPATH, "//button[contains(text(), 'Button with Dynamic ID')]")
+
+    button.click()
+
+    time.sleep(2)
+
+finally:
+    driver.quit()

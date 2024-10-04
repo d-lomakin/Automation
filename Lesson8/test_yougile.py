02fd75caf099c799c52cbadcfec57a155e15a0f8
d.lomakin <d.lomakin1705@gmail.com>
2024-10-04 17:12:54 +0300

    Update

--- Lesson8/test_yougile.py
+++ Lesson8/test_yougile.py
@@ -1,7 +1,9 @@
+import pytest
 import requests
 
 BASE_URL = "https://api.yougile.com/api-v2/projects"
-API_KEY = ""  # Вставить API ключ 
+API_KEY = "" # Вставить API ключ
+USER_ID = "" # Вставить userId
 
 
 def get_headers():
@@ -10,88 +12,84 @@ def get_headers():
         "Content-Type": "application/json"
     }
 
-
-project_id = None
-
-# Позитивные тесты
-
-
-def test_create_project():
-    global project_id
+@pytest.fixture
+def create_project():
     url = BASE_URL
     data = {
         "title": "Проверочный проект",
         "users": {
-            "": "admin"  # Вставить userId
+            USER_ID: "admin"
         }
     }
     response = requests.post(url, json=data, headers=get_headers())
-
     assert response.status_code == 201
-    assert "id" in response.json()
     project_id = response.json()["id"]
+    return project_id
 
+# Позитивные тесты
+
+def test_create_project():
+    url = BASE_URL
+    data = {
+        "title": "Новый проект",
+        "users": {
+            USER_ID: "admin"
+        }
+    }
+    response = requests.post(url, json=data, headers=get_headers())
+    assert response.status_code == 201
+    response_data = response.json()
+    assert "id" in response_data
 
 def test_get_projects():
     url = BASE_URL
     response = requests.get(url, headers=get_headers())
-
     assert response.status_code == 200
     response_data = response.json()
-
     assert 'content' in response_data
     assert isinstance(response_data['content'], list)
 
-
-def test_get_project_by_id():
-    global project_id
+def test_get_project_by_id(create_project):
+    project_id = create_project
     url = f"{BASE_URL}/{project_id}"
     response = requests.get(url, headers=get_headers())
-
     assert response.status_code == 200
     response_data = response.json()
     assert response_data["id"] == project_id
 
-
-def test_update_project():
-    global project_id
+def test_update_project(create_project):
+    project_id = create_project
     url = f"{BASE_URL}/{project_id}"
     data = {
         "title": "Измененный проект",
         "users": {
-            "": "admin"  # Вставить userId
+            USER_ID: "admin"
         }
     }
     response = requests.put(url, json=data, headers=get_headers())
-
     assert response.status_code == 200
     response_data = response.json()
-    assert "id" in response_data
     assert response_data["id"] == project_id
 
 # Негативные тесты
-
 
 def test_create_project_without_title():
     url = BASE_URL
     data = {
         "users": {
-            "": "admin"  # Вставить userId
+            USER_ID: "admin"
         }
     }
     response = requests.post(url, json=data, headers=get_headers())
-
     assert response.status_code == 400
-
 
 def test_update_project_without_id():
     url = f"{BASE_URL}/"
     data = {
-        "title": "Измененный проект без ID",
+        "title": "Проект без ID",
         "users": {
-            "": "admin"  # Вставить userId
+            USER_ID: "admin"
         }
     }
     response = requests.put(url, json=data, headers=get_headers())
-
     assert response.status_code == 404

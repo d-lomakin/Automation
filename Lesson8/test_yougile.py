ef36c946b0e9ac5fb017365f6eeffb8a7cc6b35d
d.lomakin <d.lomakin1705@gmail.com>
2024-10-10 17:28:47 +0300

    HMW9

--- Lesson8/test_yougile.py
+++ Lesson8/test_yougile.py
@@ -2,8 +2,8 @@ import pytest
 import requests
 
 BASE_URL = "https://api.yougile.com/api-v2/projects"
-API_KEY = "" # Вставить API ключ
-USER_ID = "" # Вставить userId
+API_KEY = ""  # Вставить API ключ
+USER_ID = ""  # Вставить userId
 
 
 def get_headers():
@@ -11,6 +11,7 @@ def get_headers():
         "Authorization": f"Bearer {API_KEY}",
         "Content-Type": "application/json"
     }
+
 
 @pytest.fixture
 def create_project():
@@ -28,6 +29,7 @@ def create_project():
 
 # Позитивные тесты
 
+
 def test_create_project():
     url = BASE_URL
     data = {
@@ -41,6 +43,7 @@ def test_create_project():
     response_data = response.json()
     assert "id" in response_data
 
+
 def test_get_projects():
     url = BASE_URL
     response = requests.get(url, headers=get_headers())
@@ -49,6 +52,7 @@ def test_get_projects():
     assert 'content' in response_data
     assert isinstance(response_data['content'], list)
 
+
 def test_get_project_by_id(create_project):
     project_id = create_project
     url = f"{BASE_URL}/{project_id}"
@@ -56,6 +60,7 @@ def test_get_project_by_id(create_project):
     assert response.status_code == 200
     response_data = response.json()
     assert response_data["id"] == project_id
+
 
 def test_update_project(create_project):
     project_id = create_project
@@ -73,6 +78,7 @@ def test_update_project(create_project):
 
 # Негативные тесты
 
+
 def test_create_project_without_title():
     url = BASE_URL
     data = {
@@ -82,6 +88,7 @@ def test_create_project_without_title():
     }
     response = requests.post(url, json=data, headers=get_headers())
     assert response.status_code == 400
+
 
 def test_update_project_without_id():
     url = f"{BASE_URL}/"
